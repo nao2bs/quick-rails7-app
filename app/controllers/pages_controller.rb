0dae07430560bca74faa7dbc16a1a4f2d7f4f686
NAO <nao2bs@gmail.com>
2023-03-11 23:40:11 +0900

    Add guest-login

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,20 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, only: [:home]
+
+  def index
+    @title = "index"
+  end
 
   def home
-    @title = "Home"
+    @title = "home"
+  end
+
+  def new_guest
+    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+    user.password = SecureRandom.urlsafe_base64
+    # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+    end
+    sign_in user
+    redirect_to "/pages/home", notice: 'ゲストユーザーとしてログインしました。'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
 
-  root "pages#home"
+  # http://localhost:3000
+  root "pages#index"
+
+  # http://localhost:3000/pages/home
+  get "pages/home"
   get 'set_theme', to: 'theme#update'
+
+  post '/pages/guest_sign_in', to: 'pages#new_guest'
+
 end
