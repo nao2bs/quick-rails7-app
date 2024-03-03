ff46aa6eb84aec48ba7ad4e2a0a30339ca73dab7
nao2bs <57845054+nao2bs@users.noreply.github.com>
2024-03-03 06:42:35 +0000

    scaffold Cat追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  resources :cats
   resources :owls
   resources :hedgehogs
   resources :chicks
   resources :dogs
-  resources :cats
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
